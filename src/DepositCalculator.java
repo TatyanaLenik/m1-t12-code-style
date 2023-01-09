0b868120cb9a6d41fe1464724760f3ae439213cf
TatyanaLenik <119074120+TatyanaLenik@users.noreply.github.com>
2023-01-09 14:39:12 +0300

    Add files via upload

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
 
 public class DepositCalculator {
+    public static final int depositPeriod = 12;
+    public static final double yearRate = 0.06;
+    public static final double scale= 100;
+
     /* название переменных рекомендуется называть в соответствии со смыслом предназначения переменных */
-    double calculateComplexPercent(int amount,double percent, int period) { // два метода, логично было бы назвать переменные одинаково
+    double calculateComplexPercent(int amount) { // два метода, логично было бы назвать переменные одинаково
         /* 12 - можно вынести в константу для лучшей читаемости кода,
          чтобы лучше понимать какую функцию она несет. */
-        double pay = amount * Math.pow((1 + percent/ 12), 12 *period);
+
+        double pay = amount * Math.pow((1 + yearRate/ depositPeriod), depositPeriod *yearRate);
         return round(pay);
     }
 
     /* нет смысла передавать значение - 2, так как это неизменная константа для формулы в методе rnd*/
-    double calculateSimplePercent(double amount, double yearRate, int depositPeriod) { // два метода, логично было бы назвать переменные одинаково
+    double calculateSimplePercent(int amount) {
+        // два метода, логично было бы назвать переменные одинаково
         return round(amount+amount * yearRate *depositPeriod);
     }
 
@@ -18,9 +24,9 @@ public class DepositCalculator {
         /* Во всех случаях scale не изменим и равен 100,
          ее можно вынести в константу для лучшей читаемости кода,
          чтобы лучше понимать какую функцию она несет. */
-        double scaLe= Math.pow(10); /* здесь ошибка!!!, на мой взгляд проще scale сделать константой равной 100,
+         /* здесь ошибка!!!, на мой взгляд проще scale сделать константой равной 100,
          константы: https://javarush.com/groups/posts/1557-konstantih-i-internacionalizacija */
-        return Math.round(value*scaLe) /scaLe;
+        return Math.round(value*scale) /scale;
     }
 
     void getDepositDetails() {
@@ -38,7 +44,7 @@ public class DepositCalculator {
         /* название переменной не совсем отражает смысл, рекомендую переимменовать,
          для большей читаемости кода рекомендую перенести объявление переменной
          в начало метода к остальным переменным */
-        double out = 0; // название непонятное...
+        double result = 0; // название непонятное...
 
         if (depositType ==1) {
             /* процентную ставку можно вынести в константу для лучшей читаемости кода,
@@ -46,13 +52,13 @@ public class DepositCalculator {
              сейчас она как будто из воздуха,
              при этом, нет смысла передавать константу как параметр метода,
              ее можно перенести в класс DepositCalculator */
-            out = calculateSimplePercent(amount, 0.06, period);
+            result = calculateSimplePercent(amount);
         } else if (depositType == 2) {
             /* здесь опечатка в названии метода, должен быть вызов
              calculateComplexPercent() */
-            out= calculateComplexPercent(amount, 0.06, period);
+            result = calculateComplexPercent(amount);
         }
-        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + out);
+        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + result);
     }
     public static void main(String[] args) {
         new DepositCalculator().getDepositDetails();
