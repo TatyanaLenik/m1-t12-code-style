5198d1f0f0a36c28aaadf038ff89ff63fff72546
TatyanaLenik <119074120+TatyanaLenik@users.noreply.github.com>
2023-01-09 13:41:03 +0300

    Add files via upload

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,43 +1,59 @@
 import java.util.Scanner;
 
 public class DepositCalculator {
-    double calculateComplexPercent(double a, double y, int d) {
-        double pay = a * Math.pow((1 + y/ 12), 12 *d);
-        return rnd(pay, 2);
+    /* название переменных рекомендуется называть в соответствии со смыслом предназначения переменных */
+    double calculateComplexPercent(int amount,double percent, int period) {
+        /* 12 - можно вынести в константу для лучшей читаемости кода,
+         чтобы лучше понимать какую функцию она несет. */
+        double pay = amount * Math.pow((1 + percent/ 12), 12 *period);
+        return round(pay);
     }
 
+    /* нет смысла передавать значение - 2, так как это неизменная константа для формулы в методе rnd*/
     double calculateSimplePercent(double amount, double yearRate, int depositPeriod) {
-        return rnd(amount+amount * yearRate *depositPeriod, 2);
+        return round(amount+amount * yearRate *depositPeriod);
     }
 
-    double rnd(double value, int places) {
-        double scaLe= Math.pow
-                (10, places);
-        return Math.round(value*scaLe)
-                /scaLe;
+    double round(double value) {
+        /* Во всех случаях scale не изменим и равен 100,
+         ее можно вынести в константу для лучшей читаемости кода,
+         чтобы лучше понимать какую функцию она несет. */
+        double scaLe= Math.pow(10);
+        return Math.round(value*scaLe) /scaLe;
     }
 
     void getDepositDetails() {
-        int period, action ;
+        /* Переменные желательного объявлять отдельно на каждой строчке, это повышает читабельность кода */
+        int period;
+        int depositType;
+        /* после объявления переменных рекомендуется оставлять одну пустую строку */
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите сумму вклада в рублях:") ;
         int amount = scanner.nextInt();
         System.out.println("Введите срок вклада в годах:") ;
         period = scanner.nextInt( );
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
-        action = scanner.nextInt();
+        depositType = scanner.nextInt();
+        /* название переменной не совсем отражает смысл, рекомендую переимменовать,
+         для большей читаемости кода рекомендую перенести объявление переменной
+         в начало метода к остальным переменным */
         double out = 0;
 
-        if (action ==1) {
+        if (depositType ==1) {
+            /* процентную ставку можно вынести в константу для лучшей читаемости кода,
+            чтобы лучше понимать какую функцию она несет,
+             сейчас она как будто из воздуха,
+             при этом, нет смысла передавать константу как параметр метода,
+             ее можно перенести в класс DepositCalculator */
             out = calculateSimplePercent(amount, 0.06, period);
-        } else if (action == 2) {
-            out= calculateSimplePercent(amount, 0.06, period);
+        } else if (depositType == 2) {
+            /* здесь опечатка в названии метода, должен быть вызов
+             calculateComplexPercent() */
+            out= calculateComplexPercent(amount, 0.06, period);
         }
         System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + out);
     }
-
     public static void main(String[] args) {
         new DepositCalculator().getDepositDetails();
     }
 }
-
